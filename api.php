7471b0b0d697defe26cc784eafe0d1531dd70b43
shturnev <sht_job@ukr.net>
2018-03-05 02:03:34 +0200

    +

--- api.php
+++ api.php
@@ -4,6 +4,8 @@
  * Date: 22.01.2018
  * Time: 1:01
  */
+
+
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
